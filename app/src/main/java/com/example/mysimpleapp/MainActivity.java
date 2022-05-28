0bf410fbbea1570844b477788838bec58b52bc00
ayan-de <deayan252@gmail.com>
2022-05-29 03:36:45 +0530

    accept full name and email and displays them

--- app/src/main/java/com/example/mysimpleapp/MainActivity.java
+++ app/src/main/java/com/example/mysimpleapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,8 +20,18 @@ public class MainActivity extends AppCompatActivity
     }
     public void onBtnClick(View View)
     {
-        TextView txtHello = findViewById(R.id.textMessage);
-        EditText editTextName = findViewById(R.id.editTxtName);
-        txtHello.setText("Hello "+editTextName.getText().toString());
+        EditText firstname = findViewById(R.id.FirstName);
+
+        EditText lastname = findViewById(R.id.LastName);
+
+        EditText email = findViewById(R.id.Email);
+
+        TextView txtDetail = findViewById(R.id.textView);
+        txtDetail.setText(firstname.getText().toString());
+        TextView txtDetail2 = findViewById(R.id.textView2);
+        txtDetail2.setText(lastname.getText().toString());
+        TextView txtDetail3 = findViewById(R.id.textView3);
+        txtDetail3.setText(email.getText().toString());
+
     }
 }
